543e4bc61fb891b4ab7514972e8f460de69f2ebb
Gibran Andre <gibranayala798@gmail.com>
2020-08-07 10:55:14 -0500

    Updates

---
+++ precache-manifest.372b9dac47b5e578eb33e1efc0137974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5aa384a2cc0b2e9029fbfdcc8bfeb0",
+    "url": "/react-deploy-web/index.html"
+  },
+  {
+    "revision": "d02960c5983350d0bf29",
+    "url": "/react-deploy-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fce7d4377aee0646bb88",
+    "url": "/react-deploy-web/static/js/2.6f55a699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-web/static/js/2.6f55a699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d02960c5983350d0bf29",
+    "url": "/react-deploy-web/static/js/main.995176cf.chunk.js"
+  },
+  {
+    "revision": "1d86e29288e85b47bfc0",
+    "url": "/react-deploy-web/static/js/runtime-main.03507f05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
